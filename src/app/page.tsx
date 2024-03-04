f545ae7c04632f08d189ccd1580477093b0640ba
Unstopabloo <pablooyarceramirez@gmail.com>
2024-03-03 23:56:50 -0300

    add login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,65 @@
 import Image from "next/image";
+import { Button } from "@/ui/button";
+import Link from "next/link";
+import Grid from "@/public/grid.svg"
+import Moon from "@/public/moon.png"
+import Moon2 from "@/public/moon2.png"
+import Moon3 from "@/public/moon3.png"
+import Moon4 from "@/public/moon4.png"
+import GodRays from "@/public/godrays.png"
 
-export default function Home() {
+export default function Login() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="container mx-auto p-4 flex flex-col gap-20 items-center justify-center h-screen overflow-x-hidden overflow-y-hidden relative">
+      <Image
+        className="absolute -top-48 left-1/2 transform -translate-x-1/2 -z-10"
+        src={Grid}
+        alt="Grid"
+        width={948}
+        height={948}
+      />
+      <Image
+        className="absolute right-0 top-10 -z-10 floating-vertical"
+        src={Moon}
+        alt="Moon asset"
+        width={108}
+        height={108}
+      />
+      <Image
+        className="absolute right-0 bottom-24 -z-10 floating-vertical"
+        src={Moon2}
+        alt="Moon asset"
+        width={60}
+        height={60}
+      />
+      <Image
+        className="absolute left-0 top-50 -z-10 floating-vertical"
+        src={Moon3}
+        alt="Moon asset"
+        width={33}
+        height={33}
+      />
+      <Image
+        className="absolute left-96 top-10 -z-10 floating-vertical"
+        src={Moon4}
+        alt="Moon asset"
+        width={19}
+        height={19}
+      />
+      <Image
+        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -z-10"
+        src={GodRays}
+        alt="God rays asset"
+        width={1270}
+        height={1270}
+      />
+      <div className="flex flex-col gap-8">
+        <h1 className="text-6xl font-bold max-w-[680px] text-center">Crece con inteligencia, prospera con <strong className="text-primary">KairosX</strong></h1>
+        <p className="text-white/80 px-10 max-w-[680px] text-center">KairosX es la herramienta de gestión financiera más intuitiva, segura y eficaz para el programador moderno. Te permite tomar el control de tu dinero, aprovechar al máximo cada momento y alcanzar tus metas financieras con mayor rapidez.</p>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Button asChild className="button-blob font-semibold text-lg rounded-lg px-12">
+        <Link href="/dashboard">Ingresar</Link>
+      </Button>
     </main>
   );
 }
