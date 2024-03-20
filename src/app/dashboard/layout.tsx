0ecb36698ef85d36c38ef90e490a1ce8b7640a2e
Unstopabloo <pablooyarceramirez@gmail.com>
2024-03-20 13:24:43 -0300

    add login fix styles

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { HomeIcon } from "@/ui/icons";
 import { User, Settings } from 'lucide-react';
 
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex flex-col items-center justify-center p-6 text-white h-screen overflow-hidden">
@@ -11,7 +13,7 @@ export default async function DashboardLayout({ children }: { children: React.Re
       <nav className="p-6 font-semibold flex items-center justify-center gap-10">
         <ul className="nav-list bg-[rgba(13, 15, 14)] flex items-center gap-5 px-2 py-1 rounded-lg [&>li]:list-none [&>li]:p-2">
           <Link className="w-full hover:bg-[#7bf3a97a] p-2 rounded-lg" href={"/dashboard"}><HomeIcon /></Link>
-          <Link className="w-full hover:bg-[#7bf3a97a] p-2 rounded-lg" href={"/dashboard"}><User size={25} /></Link>
+          <LogoutLink className="w-full hover:bg-[#7bf3a97a] p-2 rounded-lg"><User size={25} /></LogoutLink>
           <Link className="w-full hover:bg-[#7bf3a97a] p-2 rounded-lg" href={"/dashboard"}><Settings size={25} /></Link>
         </ul>
         <Link href="/">Añadir</Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ import Peloti from "@/public/re.webp"
 
 export default function Login() {
   return (
-    <main className="container mx-auto p-4 flex flex-col gap-20 items-center justify-center h-screen overflow-x-hidden overflow-y-hidden relative">
+    <main className="container mx-auto p-4 text-white flex flex-col gap-20 items-center justify-center h-screen overflow-x-hidden overflow-y-hidden relative">
       <Image
         className="absolute -top-48 left-1/2 transform -translate-x-1/2 -z-10"
         src={Grid}
@@ -50,7 +50,7 @@ export default function Login() {
         height={19}
       />
       <Image
-        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -z-10"
+        className="absolute -bottom-[550px] left-1/2 transform -translate-x-1/2 -z-10"
         src={GodRays}
         alt="God rays asset"
         width={1270}
@@ -60,7 +60,7 @@ export default function Login() {
         <h1 className="text-6xl font-bold max-w-[680px] text-center">Crece con inteligencia, prospera con <strong className="text-primary">KairosX</strong></h1>
         <p className="text-white/80 px-10 max-w-[680px] text-center">KairosX es la herramienta de gestión financiera más intuitiva, segura y eficaz para el programador moderno. Te permite tomar el control de tu dinero, aprovechar al máximo cada momento y alcanzar tus metas financieras con mayor rapidez.</p>
       </div>
-      <Button asChild className="button-blob font-semibold text-lg rounded-lg px-12">
+      <Button asChild className="button-blob font-semibold text-black text-lg rounded-lg px-12">
         <LoginLink>Ingresar</LoginLink>
       </Button>
     </main>
