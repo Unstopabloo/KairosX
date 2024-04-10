4bbd9171816b5f1490275b1cd2ceec679a6e2f10
Unstopabloo <pablooyarceramirez@gmail.com>
2024-04-09 22:40:27 -0400

    solving error

--- src/app/api/auth/success/route.ts
+++ src/app/api/auth/success/route.ts
@@ -6,34 +6,29 @@ export async function GET() {
   console.log('Creating user')
   const { getUser } = getKindeServerSession();
 
-  try {
-    console.log('Getting user')
+  console.log('Getting user')
 
-    const user = await getUser();
-    const kinde_id = user?.id;
-    const username = user?.given_name;
-    const email = user?.email;
-    const avatar = user?.picture;
+  const user = await getUser();
+  const kinde_id = user?.id;
+  const username = user?.given_name;
+  const email = user?.email;
+  const avatar = user?.picture;
 
-    console.log('kinde_id', kinde_id)
+  console.log('kinde_id', kinde_id)
 
-    if (typeof user?.id !== 'string') {
-      throw new Error('Invalid id');
-    }
-
-    const findUser = await sql`SELECT * FROM users WHERE kinde_id = ${kinde_id}`;
-
-    if (findUser.rowCount > 0) {
-      console.log('User already exists, returning user');
-      return NextResponse.redirect('http://localhost:3000/dashboard');
-    }
-
-    const result = await sql`INSERT INTO users (kinde_id, username, email, avatar) VALUES (${kinde_id}, ${username}, ${email}, ${avatar}) RETURNING *`;
-
-    console.log('User created successfully', result);
-    return NextResponse.redirect('http://localhost:3000/dashboard');
-  } catch (error) {
-    console.error('Error creating user', error);
-    return error;
+  if (typeof user?.id !== 'string') {
+    throw new Error('Invalid id');
   }
+
+  const findUser = await sql`SELECT * FROM users WHERE kinde_id = ${kinde_id}`;
+
+  if (findUser.rowCount > 0) {
+    console.log('User already exists, returning user');
+    return NextResponse.redirect('http://localhost:3000/dashboard');
+  }
+
+  const result = await sql`INSERT INTO users (kinde_id, username, email, avatar) VALUES (${kinde_id}, ${username}, ${email}, ${avatar}) RETURNING *`;
+
+  console.log('User created successfully', result);
+  return NextResponse.redirect('http://localhost:3000/dashboard');
 }
