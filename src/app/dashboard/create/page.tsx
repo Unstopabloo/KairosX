1aa8d2be5181da3d845475f2ea7421c57ccb8c20
Pablo Oyarce <pablooyarceramirez@gmail.com>
2024-04-26 16:50:33 -0400

    se añade logica de post de gasto e ingresos

--- src/app/dashboard/create/page.tsx
+++ src/app/dashboard/create/page.tsx
@@ -1,6 +1,6 @@
-import ListCard from './_create_components/ListCard'
-
-import AddIncomes from './_create_components/AddIncome'
+import ListCard from '@/app/dashboard/create/_create_components/ListCard'
+import AddIncomes from '@/app/dashboard/create/_create_components/AddIncome'
+import AddGastos from '@/app/dashboard/create/_create_components/AddGasto'
 
 export default async function Create() {
     return (
@@ -24,8 +24,8 @@ export default async function Create() {
                 <div className='h-fit min-h-9 w-[1px] bg-white/50'></div>
                 <article className='w-full'>
                     <header className='flex items-center justify-between w-full'>
-                        <h2 className='font-semibold text-white/90'>Ingresos</h2>
-                        <AddIncomes />
+                        <h2 className='font-semibold text-white/90'>Gastos</h2>
+                        <AddGastos />
                     </header>
                     <div className='flex flex-col gap-4 my-10'>
                         <ListCard />
---
+++ src/server/Ingresos/postIngreso.ts
@@ -0,0 +1,45 @@
+"use server"
+
+import { sql } from "@vercel/postgres";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { getUserId } from "@/server/Users/getUserId";
+import { redirect } from "next/navigation";
+
+interface Ingreso {
+  name: string;
+  value: number;
+  comes_from: string;
+  icon: string;
+  settled: Date;
+  isActive: boolean;
+  ending?: Date | undefined;
+}
+
+export async function postIngreso(values: Ingreso) {
+  console.log('post ingresos function called')
+
+  try {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+    if (!user) {
+      redirect("/")
+    }
+
+    const user_id = await getUserId();
+    if (!user_id) {
+      redirect("/")
+    }
+
+    const settled = values.settled instanceof Date ? values.settled.toISOString() : values.settled;
+    const ending = values.ending instanceof Date ? values.ending.toISOString() : values.ending;
+
+    const result = await sql`INSERT INTO ingresos (name, value, comes_from, icon, user_id, settled, isActive, ending) 
+      VALUES (${values.name}, ${values.value}, ${values.comes_from}, ${values.icon}, ${user_id}, ${settled}, ${values.isActive}, ${ending})`;
+
+    return result;
+
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}
